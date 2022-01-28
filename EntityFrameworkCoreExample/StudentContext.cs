5be5d109ced5def79f92f511d2dfb872c08d4b81
Jaein Lewis <dlwodls0115@gmail.com>
2022-01-27 16:59:15 -0800

    Created a Student model to track in the Database

--- EntityFrameworkCoreExample/StudentContext.cs
+++ EntityFrameworkCoreExample/StudentContext.cs
@@ -7,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkCoreExample
 {
-    internal class StudentContext : DbContext
+    public class StudentContext : DbContext
     {
+        public StudentContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            {
+                // Database = The desired name for the database
+                // Server = The server we are connecting to. LocalDB is included with VS
+                // Trusted_Connection = indicates that our windows account should be used
+                options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFCoreExample;Trusted_Connection=True;");
+            }
+        }
     }
-}
+
+    
